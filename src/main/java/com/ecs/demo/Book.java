98aa23e2a291c92c3dd67921135726fd735527c0
dilleswar <dilleswar1603.n@gmail.com>
2021-09-21 17:10:24 +0530

    first commit

---
+++ src/main/java/com/ecs/demo/Book.java
@@ -0,0 +1,45 @@
+package com.ecs.demo;
+
+public class Book {
+	
+	private int id;
+	private String name;
+	private String author;
+	
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(int id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+
+}
---
+++ src/main/java/com/ecs/demo/BookController.java
@@ -0,0 +1,35 @@
+package com.ecs.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class BookController {
+	
+	private static final Book[] books= {
+			new Book(1,"Nemesis","Isaac Asimov"),
+			new Book(2,"Great Expectations","Charles Dickens"),
+			new Book(3,"The Chronicles of Narnia","C.S. Lewis"),
+			new Book(4,"The Answers To Big Questions","Stephen Hawking")
+	};
+	
+	@GetMapping("/books")
+	public Book[] books() {
+		return books;
+	}
+	
+	@GetMapping("/books/{id}")
+	public Book book(@PathVariable int id) {
+		for(Book book : books) {
+			if(book.getId()==id) {
+				return book;
+			}
+		}
+		throw new ResponseStatusException(
+				HttpStatus.NOT_FOUND,"Entity Not Found");
+	}
+
+}
